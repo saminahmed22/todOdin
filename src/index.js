ae274d72d686424358934d0cfcb72592016c6de4
Samin Ahmed <saminahmed1001@gmail.com>
2025-03-22 09:10:20 +0600

    refactor: optimised the method to get modal id dynamically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {showFrom} from "./modules/forms";
+import {showform} from "./modules/forms";
 import "./styles.css";
 // Drag Logic starts here
 const draggables = document.querySelectorAll(".ToDo");
@@ -79,47 +79,13 @@ document.querySelectorAll('input[type="date"]').forEach(cal => {
 })
 
 
-// Buttons
-const addProjectBtn = document.querySelector(".addProjectBtn")
-const addTodoBtn = document.querySelector(".addTodoBtn")
-const postponeBtn = document.querySelector(".postponeBtn")
-const projectEditBtn = document.querySelector(".projectEditBtn")
-const projectDeleteBtn = document.querySelector(".projectDeleteBtn")
+// event listeners for buttons
+const buttons = document.querySelectorAll("button")
 
-const todoEditBtn = document.querySelectorAll(".todoEditBtn")
-const todoDeleteBtn = document.querySelectorAll(".todoDeleteBtn")
-
-
-addProjectBtn.addEventListener("click", () => {
-    showFrom("createProjectmodal");
-})
-
-addTodoBtn.addEventListener("click", () => {
-    showFrom("createTodomodal");
-})
-
-postponeBtn.addEventListener("click", () => {
-    showFrom("postponemodal");
-})
-
-projectEditBtn.addEventListener("click", () => {
-    showFrom("editProjectmodal");
-})
-
-projectDeleteBtn.addEventListener("click", () => {
-    showFrom("deleteProjectmodal");
-})
-
-
-
-todoEditBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        showFrom("editTodomodal");
-    })
-})
-
-todoDeleteBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        showFrom("deleteTodomodal");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modalID = button.id.slice(0, -3);
+        console.log(modalID)
+        showform(modalID);
     })
 })
